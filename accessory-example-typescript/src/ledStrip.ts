c6ad5d2960902fa07bf916c553fe81907bcd309a
Nathan Boisneault <nat.han.b@outlook.com>
2021-01-24 17:24:07 -0500

    Remove hap

--- accessory-example-typescript/src/ledStrip.ts
+++ accessory-example-typescript/src/ledStrip.ts
@@ -15,34 +15,9 @@ import {Socket} from "dgram";
 import Color from "color";
 
 /*
- * IMPORTANT NOTICE
- *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
- * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
- * and will disappear once the code is compiled to Javascript.
- * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
- * You will notice that the file does not contain a `... = require("homebridge");` statement anywhere in the code.
- *
- * The contents of the above import statement MUST ONLY be used for type annotation or accessing things like CONST ENUMS,
- * which is a special case as they get replaced by the actual value and do not remain as a reference in the compiled code.
- * Meaning normal enums are bad, const enums can be used.
- *
- * You MUST NOT import anything else which remains as a reference in the code, as this will result in
- * a `... = require("homebridge");` to be compiled into the final Javascript code.
- * This typically leads to unexpected behavior at runtime, as in many cases it won't be able to find the module
- * or will import another instance of homebridge causing collisions.
- *
- * To mitigate this the {@link API | Homebridge API} exposes the whole suite of HAP-NodeJS inside the `hap` property
- * of the api object, which can be acquired for example in the initializer function. This reference can be stored
- * like this for example and used to access all exported variables and classes from HAP-NodeJS.
- */
-let hap: HAP;
-
-/*
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  hap = api.hap;
   api.registerAccessory("LedStrip", LedStrip);
 };
 
@@ -66,26 +41,26 @@ class LedStrip implements AccessoryPlugin {
     this.name = config.name;
     this.udpClient = dgram.createSocket('udp4');
 
-    this.ledStripService = new hap.Service.Lightbulb(this.name);
-    this.ledStripService.getCharacteristic(hap.Characteristic.On)
+    this.ledStripService = new api.hap.Service.Lightbulb(this.name);
+    this.ledStripService.getCharacteristic(api.hap.Characteristic.On)
       .on(CharacteristicEventTypes.GET, this.handleOnGet.bind(this))
       .on(CharacteristicEventTypes.SET, this.handleOnSet.bind(this));
 
-    this.ledStripService.getCharacteristic(hap.Characteristic.Hue)
+    this.ledStripService.getCharacteristic(api.hap.Characteristic.Hue)
       .on(CharacteristicEventTypes.GET, this.handleHueGet.bind(this))
       .on(CharacteristicEventTypes.SET, this.handleHueSet.bind(this));
 
-    this.ledStripService.getCharacteristic(hap.Characteristic.Saturation)
+    this.ledStripService.getCharacteristic(api.hap.Characteristic.Saturation)
       .on(CharacteristicEventTypes.GET, this.handleSaturationGet.bind(this))
       .on(CharacteristicEventTypes.SET, this.handleSaturationSet.bind(this));
 
-    this.ledStripService.getCharacteristic(hap.Characteristic.Brightness)
+    this.ledStripService.getCharacteristic(api.hap.Characteristic.Brightness)
       .on(CharacteristicEventTypes.GET, this.handleBrightnessGet.bind(this))
       .on(CharacteristicEventTypes.SET, this.handleBrightnessSet.bind(this));
 
-    this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "Nathan Boisneault")
-      .setCharacteristic(hap.Characteristic.Model, "LED strip");
+    this.informationService = new api.hap.Service.AccessoryInformation()
+      .setCharacteristic(api.hap.Characteristic.Manufacturer, "Nathan Boisneault")
+      .setCharacteristic(api.hap.Characteristic.Model, "LED strip");
 
     log.info("LED Strip initialized");
   }

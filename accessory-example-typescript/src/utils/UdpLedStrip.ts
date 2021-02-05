006b542f7ccc23d98b3b2d5c4a3840374a67d1d9
Nathan Boisneault <nat.han.b@outlook.com>
2021-02-04 20:30:10 -0500

    Add color date

--- accessory-example-typescript/src/utils/UdpLedStrip.ts
+++ accessory-example-typescript/src/utils/UdpLedStrip.ts
@@ -7,7 +7,7 @@ import { EventEmitter } from 'events';
 
 const MULTICAST = '239.0.0.123';
 const PORT = 7026;
-const TIMEOUT = 5000;
+const UPDATE_FREQUENCY = 5000;
 
 export default class UdpLedStrip extends EventEmitter {
   public readonly ipAddress: string;
@@ -16,6 +16,7 @@ export default class UdpLedStrip extends EventEmitter {
   private udpClient: Socket;
   private log?: Logging;
   private currentColor?: Color;
+  private colorDate?: number;
 
   constructor(ipAddress: string, log: Logging) {
     super();
@@ -31,8 +32,7 @@ export default class UdpLedStrip extends EventEmitter {
       .on('message', (msg, rinfo) => {
         if (rinfo.address === this.ipAddress) {
           this.log?.info(`Received RGB value: ${msg.readUInt8(0)} ${msg.readUInt8(1)} ${msg.readUInt8(2)}`);
-          this.currentColor = Color.rgb(msg.readUInt8(0), msg.readUInt8(1), msg.readUInt8(2));
-          this.emit('colorChange', this.currentColor);
+          this.updateColor(Color.rgb(msg.readUInt8(0), msg.readUInt8(1), msg.readUInt8(2)));
         }
       });
     this.udpClient.bind(PORT, () => {
@@ -43,14 +43,24 @@ export default class UdpLedStrip extends EventEmitter {
 
     // Get current color and repeat every 5s
     this.fetchColor();
-    setInterval(this.fetchColor.bind(this), TIMEOUT);
+    setInterval(this.fetchColor.bind(this), UPDATE_FREQUENCY);
+  }
+
+  private updateColor(newColor: Color | undefined) {
+    this.currentColor = newColor;
+    this.colorDate = Date.now();
+    this.emit('colorChange', newColor);
   }
 
   private fetchColor(): void {
+    if (this.colorDate && Date.now() - this.colorDate > UPDATE_FREQUENCY) {
+      this.log?.info('Resetting color since no response were received');
+      this.updateColor(undefined);
+    }
     this.udpClient.send(Uint8Array.from([-1]), this.port, this.ipAddress);
   }
 
-  public on(event: 'colorChange', listener: (newColor: Color) => void): this {
+  public on(event: 'colorChange', listener: (newColor: Color | undefined) => void): this {
     return super.on(event, listener);
   }
 
